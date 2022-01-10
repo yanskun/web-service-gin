2dc349aa553da3514038580baa47f8cabd70e936
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-01-10 23:15:36 +0900

    feat: add get by id

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
@@ -29,6 +30,17 @@ func main() {
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message:": "album not found"})
 }
 
 func postAlbums(c *gin.Context) {
